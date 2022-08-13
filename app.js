e3bca2be6c40fbc7d9f34c565804d10722f91e0b
gucudmitriy <dmitiy.g@helium10.com>
2022-08-13 16:36:06 +0300

    added client

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express"),
   app = express(),
   jwt = require("jsonwebtoken"),
-  users = require("./users.json");
+  users = require("./users.json"),
+  cors = require("cors");
 
 const host = "127.0.0.1";
 const port = 7000;
 
 const tokenKey = "1a2b-3c4d-5e6f-7g8h";
 
-app.use(express.static(__dirname + "/static"));
+app.use(express.static(__dirname + "/build"));
 app.use(express.json());
+app.use(cors());
 app.use((req, res, next) => {
   if (req.headers.authorization) {
     jwt.verify(req.headers.authorization.split(" ")[1]),
@@ -33,6 +35,7 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/auth", (req, res) => {
+  console.log("body", req.body);
   for (let user of users) {
     if (req.body.login === user.login && req.body.password === user.password) {
       return res.status(200).json({
@@ -51,7 +54,7 @@ app.get("/user", (req, res) => {
   else return res.status(401).json({ message: "Not Authorized" });
 });
 
-app.get("/", (req, res) => res.render("index.html"));
+app.get("/*", (req, res) => res.sendFile(__dirname + "/build/index.html"));
 
 app.listen(port, host, () => {
   console.log(`Server listens http://${host}:${port}`);
